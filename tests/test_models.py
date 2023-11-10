899fcff55f9e43d956c48a8f3a139c036b8dbf1f
Lucas Taling <lutaling@live.co.uk>
2023-11-10 15:33:22 +0000

    Cleaned up test functions to be parameterized

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,30 +6,16 @@ import os
 import pytest
 
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
@@ -47,28 +33,28 @@ def test_load_from_json(tmpdir):
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
 
-def test_daily_max_integer():
-    """Test that checks if indeed the greatest number is selected in an array 
-        of positive inegers."""
-    from inflammation.models import daily_max
-
-    test_input = np.array([[2, 4],
-                           [6, 8],
-                           [10, 12]])
-    test_result = np.array(([10,12]))
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-def test_daily_min_integer():
-    """Test that checks if indeed the greatest number is selected in an array 
-        of positive inegers."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[2, 4], [6, 8], [10, 12]], [2, 4]),
+        ([[3, 6], [9, 12], [15, 18]], [3, 6]),
+    ]
+)
+def test_daily_min(test, expected):
+    """Test that checks if indeed the smallest number is selected in an array 
+    of positive integers."""
     from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
-    test_input = np.array([[3, 6],
-                           [9, 12],
-                           [15, 18]])
-    test_result = np.array(([3,6]))
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[4, 8], [12, 16], [20, 24]], [20, 24]),
+        ([[5, 10], [15, 20], [25, 30]], [25, 30]),
+    ]
+)
+def test_daily_max(test, expected):
+    """Test that checks if indeed the greatest number is selected in an array 
+    of positive integers."""
+    from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
