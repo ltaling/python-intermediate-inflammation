6bef1670ed43743f9d5b06205fe8749e2b22e8d8
Lucas Taling <lutaling@live.co.uk>
2023-11-10 15:00:21 +0000

    Added error test that checks if string is correctly handled in the daily_mean function

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 import os
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -29,6 +30,14 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
 
 def test_load_from_json(tmpdir):
     from inflammation.models import load_json
