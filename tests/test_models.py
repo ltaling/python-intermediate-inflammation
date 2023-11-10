0cef6a37e838847f4bfa774c236cc5b55579ce74
Lucas Taling <lutaling@live.co.uk>
2023-11-10 14:56:38 +0000

    Added daily max & min tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -37,3 +37,29 @@ def test_load_from_json(tmpdir):
         temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
+
+def test_daily_max_integer():
+    """Test that checks if indeed the greatest number is selected in an array 
+        of positive inegers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[2, 4],
+                           [6, 8],
+                           [10, 12]])
+    test_result = np.array(([10,12]))
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min_integer():
+    """Test that checks if indeed the greatest number is selected in an array 
+        of positive inegers."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[3, 6],
+                           [9, 12],
+                           [15, 18]])
+    test_result = np.array(([3,6]))
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
